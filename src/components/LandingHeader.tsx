883da86d5f0d3df35c6be2687eb71792689682b3
Ferdia O Corragain <ferdia@codenomads.nl>
2020-12-22 09:44:07 +0100

    Removed import

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import React from 'react';
-import { Button, jsx } from 'theme-ui';
+import { jsx } from 'theme-ui';
 
 const LandingHeader = () => {
     return (
@@ -13,7 +13,7 @@ const LandingHeader = () => {
             <h5>Reis tot 15 maart 2021 alleen noodzakelijk</h5>
             <h1>Wat als je toch naar het buitenland gaat?</h1>
             <h2 sx={{fontWeight: 'normal'}}>Doe de check voor corona-richtlijnen en tips voor je thuisquarantaine.</h2>
-            <Button>Doe de check</Button>
+            <button>Doe de check</button>
 
         </header>
     )
