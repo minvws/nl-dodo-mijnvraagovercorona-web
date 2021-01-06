afbb0207184e7768bcdbe9f47cd3a1678847d538
Bart Waardenburg <bart@waardenburg.dev>
2021-01-06 15:54:07 +0100

    Implement partial search

--- src/services/CountryService.ts
+++ src/services/CountryService.ts
@@ -3,9 +3,14 @@ import { countries, Country } from 'config/countries';
 const MAX_SEARCH_RESULTS = 10;
 
 export const searchCities = (query: string): Country[] => {
-	return countries
-		.filter((country) =>
-			country.fullName.toLowerCase().startsWith(query.toLowerCase()),
-		)
-		.slice(0, MAX_SEARCH_RESULTS + 1);
+	const primaryMatches = countries.filter((country) =>
+		country.fullName.toLowerCase().startsWith(query.toLowerCase()),
+	);
+	const secondaryMatches = countries.filter(
+		(country) =>
+			!primaryMatches.includes(country) &&
+			country.fullName.toLowerCase().includes(query.toLowerCase()),
+	);
+
+	return [...primaryMatches, ...secondaryMatches].slice(0, MAX_SEARCH_RESULTS);
 };
