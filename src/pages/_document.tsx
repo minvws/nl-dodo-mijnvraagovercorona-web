161d77ca2408577878f5c11b9581bd5e6999bc92
Bart Waardenburg <bart@waardenburg.dev>
2021-01-06 10:29:33 +0100

    Add favicons and other icon related metatags and files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,14 +33,11 @@ class TravelCheckDocument extends Document {
 						href="/favicon-16x16.png"
 					/>
 					<link rel="manifest" href="/site.webmanifest" />
-					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-					<meta name="msapplication-TileColor" content="#da532c" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#154273" />
+					<meta name="msapplication-TileColor" content="#154273" />
 					<meta name="theme-color" content="#ffffff" />
 				</Head>
 				<body>
-					{/* <Global styles={reset} />
-					<Global styles={fonts} />
-					<Global styles={defaults} /> */}
 					<Main />
 					<NextScript />
 
