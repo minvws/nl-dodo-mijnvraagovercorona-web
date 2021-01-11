9f9ad1751a7fbe1a6fa49b1b471d2ba3ec1bcf96
Bart Waardenburg <bart@waardenburg.dev>
2021-01-11 16:17:26 +0100

    Added feedback button to results and faqs pages

---
+++ src/components/feedback/Feedback.tsx
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import React from 'react';
+import { jsx, Styled } from 'theme-ui';
+
+import PrimaryButton from '../button/PrimaryButton';
+
+const Feedback = () => (
+	<>
+		<h2
+			sx={{
+				paddingTop: ['36px', '44px'],
+				color: 'header',
+				fontSize: ['h2Mobile', 'h2'],
+			}}
+		>
+			Waar zou je nog meer mee geholpen zijn?
+		</h2>
+		<Styled.p>
+			Help deze website beter te maken. Vermeld geen persoonsgegevens. Deze
+			worden direct verwijderd.
+		</Styled.p>
+		<PrimaryButton href="https://valsplat.typeform.com/to/hlgYe4qs" external>
+			Laat het ons weten
+		</PrimaryButton>
+	</>
+);
+
+export default Feedback;
