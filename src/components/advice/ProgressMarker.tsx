05053e380462ef7777316adb72d9c952b82dc79c
Jeroen Reumkens <hello@jeroenreumkens.nl>
2021-01-12 16:09:26 +0100

    Added lines between step circles.

--- src/components/advice/ProgressMarker.tsx
+++ src/components/advice/ProgressMarker.tsx
@@ -3,53 +3,71 @@ import React from 'react';
 import { jsx } from 'theme-ui';
 
 type ProgressMarkerProps = {
-    stage: number,
-    totalStages: number
-}
+	stage: number;
+	totalStages: number;
+};
 
 const ProgressMarker = (props: ProgressMarkerProps) => {
-    const rangeArr = [...Array(props.totalStages).keys()];
-    return (
-        <ul sx={{
-            position: 'absolute',
-            top: ['25px', '150px'],
-            paddingTop: 0,
-            marginTop: 0,
-            paddingLeft: 0,
-            fontSize: '16px',
-            fontWeight: 'bold'
-        }}>
+	const rangeArr = [...Array(props.totalStages).keys()];
+	return (
+		<ul
+			sx={{
+				position: 'absolute',
+				top: ['25px', '150px'],
+				paddingTop: 0,
+				marginTop: 0,
+				paddingLeft: 0,
+				fontSize: '16px',
+				fontWeight: 'bold',
+			}}
+		>
+			{rangeArr.map((index) => {
+				const stageNumber = index + 1;
+				const isCurrentStage = stageNumber === props.stage;
+				const isPastStage = stageNumber < props.stage;
+				let background = 'white';
 
-            {rangeArr.map(index => {
-                const stageNumber = index + 1;
-                const isCurrentStage = stageNumber === props.stage;
-                const isPastStage = stageNumber < props.stage;
-                let background =  'white';
+				if (isCurrentStage) {
+					background = 'button';
+				}
+				if (isPastStage) {
+					background = '#AEC1D1';
+				}
 
-                if (isCurrentStage) {
-                    background = 'button';
-                }
-                if (isPastStage) {
-                    background = '#AEC1D1';
-                }
-
-                return (
-                    <li sx={{
-                        display: 'inline-block',
-                        backgroundColor: background,
-                        color: isCurrentStage ? 'white' : 'header',
-                        borderRadius: '50%',
-                        width: '35px',
-                        height: '34px',
-                        padding: '8px',
-                        textAlign: 'center',
-                        marginRight: '25px'
-                    }}
-                        key={stageNumber}>{stageNumber}</li>
-                )
-            })}
-        </ul>
-    )
+				return (
+					<li
+						sx={{
+							display: 'inline-block',
+							backgroundColor: background,
+							color: isCurrentStage ? 'white' : 'header',
+							borderRadius: '50%',
+							width: '35px',
+							height: '34px',
+							padding: '8px',
+							textAlign: 'center',
+							marginRight: '25px',
+							position: 'relative',
+							'::before': {
+								content: '""',
+								display: stageNumber !== 1 ? 'block' : 'none',
+								width: '25px',
+								height: '4px',
+								backgroundColor:
+									isPastStage || isCurrentStage ? 'stepElement' : 'white',
+								left: '-25px',
+								top: '50%',
+								transform: 'translateY(-2px)',
+								position: 'absolute',
+							},
+						}}
+						key={stageNumber}
+					>
+						{stageNumber}
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default ProgressMarker;
