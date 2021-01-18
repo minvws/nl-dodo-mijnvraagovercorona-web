7ef20ff2ca9607f8c0c4fffb6847266028c92794
Bart Waardenburg <bart@waardenburg.dev>
2021-01-18 13:24:22 +0100

    Remove pilot information page and banner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 
-import { Alert, jsx, ThemeProvider, Flex } from 'theme-ui';
+import { jsx, ThemeProvider } from 'theme-ui';
 
 import { AdviceProvider } from 'components/advice/AdviceContext';
 
@@ -31,22 +31,7 @@ const TravelCheckApp = ({ Component, pageProps }: AppProps) => {
 			</Head>
 			<ThemeProvider theme={theme}>
 				<AdviceProvider>
-					<Flex sx={{ flexDirection: 'column', minHeight: '100vh' }}>
-						<Alert
-							sx={{
-								backgroundColor: 'button',
-								borderRadius: 0,
-								fontSize: '14px',
-								padding: '4px',
-							}}
-						>
-							<p sx={{ margin: 0 }}>
-								Deze site is nog in de testfase. Help ons via de knop ‘deel je
-								mening’.
-							</p>
-						</Alert>
-						<Component {...pageProps} />
-					</Flex>
+					<Component {...pageProps} />
 				</AdviceProvider>
 			</ThemeProvider>
 		</>
