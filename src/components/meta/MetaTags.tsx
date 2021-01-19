bd9e6bb1ccdfa99e5eaa4f216d6d229ac0d058e5
Bart Waardenburg <bart@waardenburg.dev>
2021-01-19 13:52:44 +0100

    Add canonical for all pages since it seems to be best SEO practive

--- src/components/meta/MetaTags.tsx
+++ src/components/meta/MetaTags.tsx
@@ -7,29 +7,29 @@ type Props = {
 	url: string;
 };
 
-const MetaTags = ({ title, description, url }: Props) => (
-	<Head>
-		<title>{title}</title>
-		<meta name="title" content={title} />
-		<meta name="description" content={description} />
+const MetaTags = ({ title, description, url }: Props) => {
+	const completeURl = `https://www.reizentijdenscorona.nl${url}`;
 
-		<meta property="og:type" content="website" />
-		<meta property="og:locale" content="nl_NL" />
-		<meta
-			property="og:url"
-			content={`https://www.reizentijdenscorona.nl${url}`}
-		/>
-		<meta property="og:title" content={title} />
-		<meta property="og:description" content={description} />
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name="title" content={title} />
+			<meta name="description" content={description} />
 
-		<meta property="twitter:card" content="summary_large_image" />
-		<meta
-			property="twitter:url"
-			content={`https://www.reizentijdenscorona.nl${url}`}
-		/>
-		<meta property="twitter:title" content={title} />
-		<meta property="twitter:description" content={description} />
-	</Head>
-);
+			<meta property="og:type" content="website" />
+			<meta property="og:locale" content="nl_NL" />
+			<meta property="og:url" content={completeURl} />
+			<link rel="canonical" href={completeURl} />
+
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content={completeURl} />
+			<meta property="twitter:title" content={title} />
+			<meta property="twitter:description" content={description} />
+		</Head>
+	);
+};
 
 export default MetaTags;
