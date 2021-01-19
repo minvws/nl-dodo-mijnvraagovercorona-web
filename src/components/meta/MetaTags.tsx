3040ecc475a15905c5ca2d020c3d1c70c599aeac
Bart Waardenburg <bart@waardenburg.dev>
2021-01-19 15:19:08 +0100

    Add share image

--- src/components/meta/MetaTags.tsx
+++ src/components/meta/MetaTags.tsx
@@ -19,6 +19,10 @@ const MetaTags = ({ title, description, url }: Props) => {
 			<meta property="og:type" content="website" />
 			<meta property="og:locale" content="nl_NL" />
 			<meta property="og:url" content={completeURl} />
+			<meta
+				property="og:image"
+				content="https://www.reizentijdenscorona.nl/share.png"
+			/>
 			<link rel="canonical" href={completeURl} />
 
 			<meta property="og:title" content={title} />
@@ -28,6 +32,10 @@ const MetaTags = ({ title, description, url }: Props) => {
 			<meta property="twitter:url" content={completeURl} />
 			<meta property="twitter:title" content={title} />
 			<meta property="twitter:description" content={description} />
+			<meta
+				property="twitter:image"
+				content="https://www.reizentijdenscorona.nl/share.png"
+			/>
 		</Head>
 	);
 };
