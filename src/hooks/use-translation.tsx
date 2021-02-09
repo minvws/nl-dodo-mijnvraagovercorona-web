399d1f000c3f31c6d64533c33b9545925a6f7de5
Jeroen Reumkens <hello@jeroenreumkens.nl>
2021-02-09 11:59:03 +0100

    Added comments to translation logic hooks and context.

--- src/hooks/use-translation.tsx
+++ src/hooks/use-translation.tsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo, createContext, useState } from 'react';
+import { useContext, useMemo, createContext, useState, useEffect } from 'react';
 // @TODO: Not really happy with processing HTML, who knows what we will be injecting...
 import parse from 'html-react-parser';
 
@@ -14,27 +14,66 @@ export type TranslationFunctionToString = (
 	variables?: TranslationVariables,
 ) => string;
 
+/**
+ * General info about setting up translations on your page:
+ *
+ * 1. Add a property 'content' in your page's staticProps. Returning
+ * an object with translation keys and their corresponding values.
+ * 2. They will automatically be picked up by _app.tsx, putting them into
+ * the translation context.
+ * 3. Use useTranslation() everywhere in your page (or nested components).
+ * 4. If a component or page uses useTranslation without content being provided
+ * via the staticProps, it will throw an error.
+ */
+
 interface UseTranslation {
 	t: TranslationFunction;
 	t_s: TranslationFunctionToString;
 }
 
+// Regex used for interpolating variables.
 const curlyBracketRegex = /\{\{(.+?)\}\}/g;
 
+/**
+ * Translation hook to get specific content based on
+ * translation keys. Replaces HTML with React components,
+ * and interpolates any variables between two curly brackets
+ * with the corresponding value; {{ variableName }}
+ *
+ * This hook returns two methods. Both accepting 2 arguments:
+ * 1. The translation key (string).
+ * 2. An optional object with variables that should be interpolated.
+ *
+ * t: Being the translation method that returns
+ * your content, while also parsing any HTML in there (converting
+ * things like <strong> to React components).
+ *
+ * t_s: Using the t() method, but after that rendering everything
+ * to a string. This is useful for things like attributes or places
+ * where you need to have a string. By mindful when using this, if there is a
+ * <strong> tag in the translation, it will simply convert to object to a string,
+ * resulting in a bad user experience.
+ */
 export const useTranslation = (): UseTranslation => {
 	const content = useContext(TranslationContext);
+
+	// Throw an error if hook is used without content.
+	useEffect(() => {
+		if (!content || Object.keys(content).length === 0)
+			throw new Error(
+				`Using useTranslation hook while no content is supplied.`,
+			);
+	}, [content]);
+
 	const t: TranslationFunction = useMemo(() => {
 		return (contentKey, variables) => {
-			if (!content)
-				throw new Error(
-					`Using useTranslation hook while no content is supplied.`,
-				);
-
 			const lookedupContent = content[contentKey];
 
+			// Throw an error if content key is not found.
 			if (!lookedupContent)
 				throw new Error(`Content with key "${contentKey}" not found."`);
 
+			// Interpolate all variables.
 			const contentWithVariables = lookedupContent.replace(
 				curlyBracketRegex,
 				(_string, variableName) => {
@@ -49,17 +88,17 @@ export const useTranslation = (): UseTranslation => {
 				},
 			);
 
+			// Convert HTML into React components and return that.
 			return parse(contentWithVariables);
 		};
 	}, []);
 
 	return {
 		t,
-		t_s: (contentKey, variables) => {
-			const translatedText = t(contentKey, variables);
-			if (translatedText === null) return translatedText;
-			return `${translatedText}`;
-		},
+		// The t_s method used the t() method, and converts it to a string.
+		// Be carefull with putting any HTML into these strings. It will
+		// turn it into an unreadable string.
+		t_s: (contentKey, variables) => `${t(contentKey, variables)}`,
 	};
 };
 
@@ -68,7 +107,10 @@ export interface TranslationObject {
 }
 
 export const TranslationContext = createContext<TranslationObject>({});
-
+/**
+ * Translation Provider used in the _app.tsx. Providing the useTranslation()
+ * hook with the translation keys and their content.
+ */
 export const TranslationProvider: React.FC<{ content?: TranslationObject }> = ({
 	children,
 	content,
