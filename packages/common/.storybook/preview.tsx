de69f31b5567fcac9098a875eaa97b04403c7009
Jeroen Reumkens <hello@jeroenreumkens.nl>
2021-03-15 13:52:59 +0100

    RTC-242: Added button variant to link component and implemented it in reizen app.

--- packages/common/.storybook/preview.tsx
+++ packages/common/.storybook/preview.tsx
@@ -3,6 +3,7 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider, jsx } from 'theme-ui';
 import { withNextRouter } from 'storybook-addon-next-router';
 import { theme } from '../src/theme';
+import '../src/theme/fonts.css';
 
 addDecorator(withNextRouter());
 addDecorator((storyFn) => (
--- packages/common/src/components/index.ts
+++ packages/common/src/components/index.ts
@@ -1,3 +1,2 @@
 export * from './layout';
-export * from './button';
 export * from './link';
