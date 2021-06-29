64e0f149d0f7b9bc134e793dc391e0cf67bb9ca4
Bart Waardenburg <bart@waardenburg.dev>
2021-06-29 10:58:46 +0200

    Ola supermercado

--- packages/cms/src/utilities/locales.ts
+++ packages/cms/src/utilities/locales.ts
@@ -1,14 +1,15 @@
 export interface Locale {
-  id: string;
-  title: string;
-  isDefault?: boolean;
+	id: string;
+	title: string;
+	isDefault?: boolean;
 }
 
 export const supportedLocales: Locale[] = [
-  { id: "nl", title: "Nederlands", isDefault: true },
-  { id: "en", title: "English" },
+	{ id: 'nl', title: 'Nederlands', isDefault: true },
+	{ id: 'en', title: 'Engels' },
+	{ id: 'es', title: 'Spaans' },
 ];
 
 export const baseLocale: Locale | undefined = supportedLocales.find(
-  (locale: Locale): boolean | undefined => locale.isDefault
+	(locale: Locale): boolean | undefined => locale.isDefault,
 );
