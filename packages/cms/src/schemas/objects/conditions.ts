4bf4945f159993136ac853e7ae5560584b1860fd
Jeroen Reumkens <hello@jeroenreumkens.nl>
2021-07-21 13:06:09 +0200

    RTC-406: Removed travel stages and dayshome.

--- packages/cms/src/schemas/objects/conditions.ts
+++ packages/cms/src/schemas/objects/conditions.ts
@@ -4,24 +4,18 @@ export default {
 	type: 'object',
 	fields: [
 		{
-			title: 'Reisfase',
-			name: 'travelStage',
-			type: 'array',
-			of: [{ type: 'reference', to: [{ type: 'travel-fase-document' }] }],
-		},
-		{
 			title: 'Risico Categorie',
 			name: 'riskCategory',
 			type: 'array',
 			of: [{ type: 'reference', to: [{ type: 'risk-category-document' }] }],
 		},
-				{
+		{
 			title: 'Risico Categorie Tussenstop Kort',
 			name: 'stopoverShort',
 			type: 'array',
 			of: [{ type: 'reference', to: [{ type: 'risk-category-document' }] }],
 		},
-				{
+		{
 			title: 'Risico Categorie Tussenstop Lang',
 			name: 'stopoverLong',
 			type: 'array',
@@ -36,15 +30,10 @@ export default {
 			],
 		},
 		{
-			title: 'Dagen sinds thuiskomst',
-			name: 'daysHome',
-			type: 'array',
-			of: [{ type: 'number' }],
-		},
-		{
 			title: 'Gebruik hoogste risico categorie',
 			name: 'useHighestRiskCategory',
-			description: 'Gebruik voor de conditie "Risico Categorie" de hoogste risico categorie tussen zowel de tussenstop als de eindbestemming',
+			description:
+				'Gebruik voor de conditie "Risico Categorie" de hoogste risico categorie tussen zowel de tussenstop als de eindbestemming',
 			type: 'boolean',
 		},
 		{
