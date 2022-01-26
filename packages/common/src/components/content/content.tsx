217dd99807de80dc8740a4bb2110e0c6a0a059df
Bram Smulders <source@bram.is>
2022-01-26 14:57:21 +0100

    Refactored content container to accept more items in the aside

--- packages/common/src/components/content/content.tsx
+++ packages/common/src/components/content/content.tsx
@@ -11,12 +11,14 @@ import {
 interface ContentProps {
 	noSpacingOnTop?: boolean;
 	hidePrivacyOnMobile?: boolean;
+	asideChildren?: React.ReactNode;
 }
 
 export const Content: React.FC<ContentProps> = ({
 	children,
 	noSpacingOnTop,
 	hidePrivacyOnMobile,
+	asideChildren,
 }) => {
 	const siteSettings = useSanitySiteSettings();
 
@@ -40,18 +42,22 @@ export const Content: React.FC<ContentProps> = ({
 					as="aside"
 					sx={{
 						margin: ['0 0 55px 0', '55px 0 55px auto'],
+						paddingLeft: ['mobilePadding', 0, 0],
+						paddingRight: ['mobilePadding', 'tabletPadding', 0],
 						maxWidth: ['100%', '340px'],
+						'> * ~ *': {
+							marginBlockStart: '40px',
+						},
 					}}
 				>
-					<BodyContainer
+					{asideChildren}
+					<div
 						sx={{
-							paddingLeft: ['mobilePadding', 0, 0],
-							paddingRight: ['mobilePadding', 'tabletPadding', 0],
 							display: hidePrivacyOnMobile ? ['none', 'block'] : undefined,
 						}}
 					>
 						<BannerDataProtection content={siteSettings.privacy} />
-					</BodyContainer>
+					</div>
 				</Box>
 			</Flex>
 		</Container>
