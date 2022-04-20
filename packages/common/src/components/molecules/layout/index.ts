03e7aba1a7fa1a68ea03639fd23972f1def79848
Bram Smulders <source@bram.is>
2022-04-20 15:00:34 +0200

    Added layout fit for button lists

--- packages/common/src/components/molecules/layout/index.ts
+++ packages/common/src/components/molecules/layout/index.ts
@@ -3,3 +3,4 @@ export { Retain } from './retain';
 export { TheSidebar } from './the-sidebar';
 export { TheSwitcher, TheSwitcherItem } from './the-switcher';
 export { TheThirds } from './the-thirds';
+export { LayoutFit } from './layout-fit';
