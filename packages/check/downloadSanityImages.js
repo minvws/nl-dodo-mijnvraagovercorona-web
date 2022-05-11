e57fc158585d1448254446ba36807211a701560d
Bram Smulders <source@bram.is>
2022-05-11 13:17:17 +0200

    Add sha1hash to downloaded sanity image filename

--- packages/check/downloadSanityImages.js
+++ packages/check/downloadSanityImages.js
@@ -8,7 +8,7 @@ const pipeline = promisify(stream.pipeline);
 (async () => {
 	try {
 		const { result } = await got(
-			'https://yiy91tbc.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22sanity.imageAsset%22%5D%7B%0A%20%20url%2C%0A%20%20originalFilename%0A%7D',
+			'https://yiy91tbc.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22sanity.imageAsset%22%5D%7B%0A%20%20url%2C%0A%20%20sha1hash%2C%0A%20%20originalFilename%0A%7D',
 			{
 				headers: {
 					'user-agent': undefined,
@@ -19,10 +19,12 @@ const pipeline = promisify(stream.pipeline);
 		rmSync('./public/images/sanity/', { recursive: true, force: true });
 		mkdirSync('./public/images/sanity/');
 
-		for (const { url, originalFilename } of result) {
+		for (const { url, sha1hash, originalFilename } of result) {
 			await pipeline(
 				got.stream(url),
-				createWriteStream(`./public/images/sanity/${originalFilename}`),
+				createWriteStream(
+					`./public/images/sanity/${sha1hash}-${originalFilename}`,
+				),
 			);
 		}
 	} catch (error) {
