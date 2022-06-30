2eacad6efbf886ac83cdfb6f567a2ea744be0d89
Bram Smulders <source@bram.is>
2022-06-30 15:46:57 +0200

    Created asnwer switch component

--- packages/mvoc/src/components/molecules/forms/index.ts
+++ packages/mvoc/src/components/molecules/forms/index.ts
@@ -4,6 +4,8 @@ export { FormAnswersDate } from './answers-date';
 export { FormAnswersAge } from './answers-age';
 export { FormAnswersButtons } from './answers-buttons';
 export { FormSubmit } from './submit';
+export { AnswerSwitch } from './answer-switch';
 export type { FormAnswersMultipleProps } from './answers-multiple';
 export type { FormAnswersSingleProps } from './answers-single';
 export type { FormSubmitProps } from './submit';
+export type { AnswerSwitchProps } from './answer-switch';
