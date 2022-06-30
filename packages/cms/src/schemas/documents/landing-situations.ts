305cecb77e6bf7899f2c58efff6c7a923a6809ac
Bram Smulders <source@bram.is>
2022-06-30 14:12:56 +0200

    Created CMS entry for situation landing pages

---
+++ packages/cms/src/schemas/documents/landing-situations.ts
@@ -0,0 +1,63 @@
+export default {
+	title: 'Landingpagina situaties',
+	name: 'landing-situations-document',
+	type: 'document',
+	preview: {
+		select: {
+			title: 'header.title.nl',
+			subtitle: 'slug.current',
+			media: 'header.image',
+		},
+	},
+	fields: [
+		{
+			title: 'Meta data',
+			name: 'metaData',
+			type: 'pageMetaData',
+		},
+		{
+			title: 'Header',
+			name: 'header',
+			type: 'object',
+			fields: [
+				{
+					title: 'Titel',
+					name: 'title',
+					type: 'localeString',
+				},
+				{
+					title: 'Chapeau',
+					name: 'chapeau',
+					type: 'localeString',
+				},
+				{
+					title: 'Content',
+					name: 'content',
+					type: 'localeBlock',
+				},
+				{
+					title: 'Illustratie',
+					name: 'image',
+					type: 'image',
+				},
+			],
+		},
+		{
+			title: 'Vraag referentie',
+			name: 'situationReference',
+			type: 'reference',
+			to: [{ type: 'situation-question-document' }],
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			description:
+				'Pas op! Wanneer je wijzigt hebben we geen redirect vanuit de vorige url',
+			options: {
+				source: 'header.title.nl',
+			},
+			validation: (Rule: any) => Rule.required(),
+		},
+	],
+};
