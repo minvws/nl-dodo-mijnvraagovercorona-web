d2b790cfc92320bc27f46d1877875c79726f9367
Bram Smulders <source@bram.is>
2022-07-12 14:58:17 +0200

    Added storyExtended object to CMS

---
+++ packages/cms/src/schemas/objects/storyExtended.ts
@@ -0,0 +1,56 @@
+export default {
+	title: 'Verhaal uitgebreid',
+	name: 'storyExtended',
+	type: 'object',
+	preview: {
+		select: {
+			title: 'title.nl',
+			subtitle: 'overview.title.nl',
+			media: 'overview.icon',
+		},
+	},
+	fields: [
+		{
+			title: 'Titel',
+			name: 'title',
+			type: 'localeString',
+		},
+		{
+			title: 'Content',
+			name: 'content',
+			type: 'localeBlock',
+		},
+		{
+			title: 'Afbeelding',
+			name: 'image',
+			type: 'image',
+		},
+		{
+			title: 'Video',
+			name: 'video',
+			type: 'video',
+		},
+		{
+			title: 'Situaties',
+			name: 'situations',
+			type: 'questionSelector',
+		},
+		{
+			title: 'Overzicht',
+			name: 'overview',
+			type: 'object',
+			fields: [
+				{
+					title: 'Titel',
+					name: 'title',
+					type: 'localeString',
+				},
+				{
+					title: 'Icoon',
+					name: 'icon',
+					type: 'image',
+				},
+			],
+		},
+	],
+};
