366410725090b20bd9bca8a27a023b06aac65d34
Bram Smulders <source@bram.is>
2022-07-13 15:43:28 +0200

    Accept `as` as prop for the grid, which allows us to use any element as the root element

--- packages/common/src/components/molecules/layout/the-grid.tsx
+++ packages/common/src/components/molecules/layout/the-grid.tsx
@@ -1,28 +1,38 @@
 /** @jsx jsx */
-import React from 'react';
+import React, { ElementType } from 'react';
 import { Box, jsx, SxStyleProp } from 'theme-ui';
 
 interface OwnProps {
+	as?: ElementType;
 	gap?: string | string[];
 	minItemSize?: string;
 	placement?: 'auto-fill' | 'auto-fit';
+	styles?: SxStyleProp;
 }
 
 export const TheGrid: React.FC<OwnProps> = ({
+	as = 'div',
 	minItemSize,
 	placement = 'auto-fill',
 	gap = ['2.25rem'],
+	styles,
 	children,
 }) => {
 	return (
 		<Box
+			as={as}
+			role={as === 'ul' || as === 'ol' ? 'list' : undefined}
 			sx={{
+				...styles,
 				display: 'grid',
 				gridTemplateColumns: `repeat(
 					${placement},
 					minmax(min(${minItemSize || '16rem'}, 100%), 1fr)
 				)`,
 				gap: gap,
+				// Reset possible list styles
+				listStyle: 'none',
+				paddingInlineStart: 0,
 			}}
 		>
 			{children}
