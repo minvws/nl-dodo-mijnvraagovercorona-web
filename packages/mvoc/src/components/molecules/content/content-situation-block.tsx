e5cb12b780434b8494d527f0c1fcb301daeaf79e
Bram Smulders <source@bram.is>
2022-07-14 10:49:06 +0200

    Content block no old situations anymore

--- packages/mvoc/src/components/molecules/content/content-situation-block.tsx
+++ packages/mvoc/src/components/molecules/content/content-situation-block.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { ContentBlock, Link, Stack } from '@quarantaine/common';
-import { Situation, SituationNew } from 'config/situaties';
+import { Situation } from 'config/situaties';
 import React from 'react';
 import { jsx } from 'theme-ui';
 
@@ -8,14 +8,9 @@ interface SituationAsLink extends Situation {
 	situationLinkTitle: string;
 }
 
-interface SituationNewAsLink extends SituationNew {
-	situationLinkTitle: string;
-}
-
 export interface ContentSituationBlockProps {
 	content?: Array<Object>;
 	situation?: SituationAsLink;
-	situationNew?: SituationNewAsLink;
 }
 
 interface OwnProps {
@@ -23,18 +18,6 @@ interface OwnProps {
 }
 
 export const getUrlBySituation = (situation: Situation) => {
-	if (
-		typeof situation.showExceptions !== 'undefined' &&
-		situation.showExceptions
-	) {
-		return `/${situation.url}/ben-ik-uitgezonderd`;
-	} else if (typeof situation.showDate !== 'undefined' && situation.showDate) {
-		return `/${situation.url}/wanneer`;
-	}
-	return `/${situation.url}`;
-};
-
-export const getUrlBySituationNew = (situation: SituationNew) => {
 	const prefix = '/situatie';
 
 	if (situation.start) return `${prefix}/${situation.start}`;
@@ -46,31 +29,20 @@ export const ContentSituationBlock: React.FC<OwnProps> = ({
 	contentBlocks,
 }) => {
 	return (
-		<Stack spacing={['16px']}>
+		<Stack
+			spacing={['1rem']}
+			styles={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}
+		>
 			{contentBlocks.map(
 				(contentBlock: ContentSituationBlockProps, key: number) => {
 					if (contentBlock.content) {
-						return (
-							<div key={key}>
-								<ContentBlock content={contentBlock.content} />
-							</div>
-						);
-					} else if (contentBlock.situationNew?.slug) {
+						return <ContentBlock key={key} content={contentBlock.content} />;
+					} else if (contentBlock.situation?.path) {
 						return (
 							<Link
 								key={key}
 								styledAs="button"
-								href={getUrlBySituationNew(contentBlock.situationNew)}
-							>
-								{contentBlock.situationNew.situationLinkTitle}
-							</Link>
-						);
-					} else if (contentBlock.situation?.url) {
-						return (
-							<Link
-								key={key}
-								styledAs="button"
-								href={getUrlBySituation(contentBlock.situation)}
+								href={`/${contentBlock.situation.path}`}
 							>
 								{contentBlock.situation.situationLinkTitle}
 							</Link>
