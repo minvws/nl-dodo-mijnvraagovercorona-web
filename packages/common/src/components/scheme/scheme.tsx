e27c2a1cead675457a0bc09d753e1cdf645509e1
Bart Waardenburg <bart@waardenburg.dev>
2022-08-10 17:23:58 +0200

    Fix link color in scheme

--- packages/common/src/components/scheme/scheme.tsx
+++ packages/common/src/components/scheme/scheme.tsx
@@ -93,7 +93,6 @@ export const SchemeBlock: React.FC<SchemeBlockProps> = ({
 				<Box
 					sx={{
 						'a, button': {
-							color: 'text',
 							textDecoration: 'underline',
 							fontSize: 'inherit',
 							fontWeight: 'inherit',
