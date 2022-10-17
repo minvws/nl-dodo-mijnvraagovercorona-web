7ed66aa67453b80a291198557c15a49b4ee35519
Bram Smulders <source@bram.is>
2022-10-17 18:13:59 +0200

    Added date utilities

---
+++ packages/mvoc-astro/src/utilities/locale/date.ts
@@ -0,0 +1,37 @@
+import { addDays, isBefore } from 'date-fns';
+
+export const isMoreThanWeekBefore = (date?: Date): boolean =>
+	date !== undefined && isBefore(new Date(), addDays(date, -7));
+
+export const formatShortDate = (
+	date: Date,
+	locale: string,
+	dayOfTheWeek: boolean,
+): string =>
+	date
+		? date
+				.toLocaleDateString(locale.replace('_', '-'), {
+					month: 'short',
+					day: 'numeric',
+					...(dayOfTheWeek ? { weekday: 'short' } : {}),
+				})
+				.replace('.', '')
+		: '';
+
+export const formatDate = (date: Date, locale: string): string =>
+	date
+		? date
+				.toLocaleDateString(locale.replace('_', '-'), {
+					month: 'long',
+					day: 'numeric',
+					weekday: 'long',
+				})
+				.replace('.', '')
+		: '';
+
+export const formatLongDate = (date: Date, locale: string) =>
+	date.toLocaleDateString(locale.replace('_', '-'), {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
