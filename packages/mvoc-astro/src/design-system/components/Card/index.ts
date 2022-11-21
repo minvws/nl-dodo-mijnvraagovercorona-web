7cc1e9117e3c482ed2b98342a8387951b6e54fb7
Bram Smulders <source@bram.is>
2022-11-21 15:52:34 +0100

    Added CardAdvice

--- packages/mvoc-astro/src/design-system/components/Card/index.ts
+++ packages/mvoc-astro/src/design-system/components/Card/index.ts
@@ -1,4 +1,5 @@
 export { default as Card } from './Card.astro';
 export { default as CardDay } from './CardDay.astro';
 export { default as CardBlock } from './CardBlock.astro';
+export { default as CardAdvice } from './CardAdvice.astro';
 export { default as Steps } from './Steps.astro';
