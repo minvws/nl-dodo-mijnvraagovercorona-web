de6ad540e1cff81c3c9e759882ac4f81342cb24b
Bram Smulders <source@bram.is>
2022-11-21 15:39:51 +0100

    Added Different cards

--- packages/mvoc-astro/src/design-system/components/Card/index.ts
+++ packages/mvoc-astro/src/design-system/components/Card/index.ts
@@ -1,2 +1,4 @@
 export { default as Card } from './Card.astro';
+export { default as CardDay } from './CardDay.astro';
+export { default as CardBlock } from './CardBlock.astro';
 export { default as Steps } from './Steps.astro';
