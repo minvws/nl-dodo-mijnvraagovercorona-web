fedf3e8acae1d0d623ed8c863f159a6403e03af6
Bram Smulders <source@bram.is>
2022-11-24 14:40:33 +0100

    Added car privacy block

--- packages/mvoc-astro/src/design-system/components/Card/index.ts
+++ packages/mvoc-astro/src/design-system/components/Card/index.ts
@@ -2,4 +2,5 @@ export { default as Card } from './Card.astro';
 export { default as CardDay } from './CardDay.astro';
 export { default as CardBlock } from './CardBlock.astro';
 export { default as CardAdvice } from './CardAdvice.astro';
+export { default as CardPrivacy } from './CardPrivacy.astro';
 export { default as Steps } from './Steps.astro';
