4d5b2480cf0acd75d3015ee06d8de770f7fab072
Bram Smulders <source@bram.is>
2022-12-19 15:33:29 +0100

    Disable preview view in sanity until we enable previews with astro

--- packages/cms/src/utilities/getViews.tsx
+++ packages/cms/src/utilities/getViews.tsx
@@ -1,7 +1,7 @@
 import S from '@sanity/desk-tool/structure-builder';
 import { MapPreview } from './MapPreview';
 import { DocumentJsonPreview } from './views/DocumentJsonPreview';
-import { WebPreview } from './views/WebPreview';
+// import { WebPreview } from './views/WebPreview';
 
 export const getViews = (type: 'page' | 'document', schemaType: string) => {
 	const views = [
@@ -9,8 +9,8 @@ export const getViews = (type: 'page' | 'document', schemaType: string) => {
 		S.view.component(DocumentJsonPreview).title('JSON'),
 	];
 
-	if (type === 'page')
-		views.push(S.view.component(WebPreview).title('Preview'));
+	// if (type === 'page')
+	// 	views.push(S.view.component(WebPreview).title('Preview'));
 	if (schemaType === 'land-document')
 		views.push(S.view.component(MapPreview).title('Risicokaart'));
 
