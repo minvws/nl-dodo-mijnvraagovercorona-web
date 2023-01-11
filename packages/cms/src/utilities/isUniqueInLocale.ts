320b9c8482af123c35b61a94eee00dc9b20a8cf7
Bram Smulders <source@bram.is>
2023-01-11 16:52:37 +0100

    create is unique in locale function to test slugs against other pages in a type or all pages in the CMS in a specific language

---
+++ packages/cms/src/utilities/isUniqueInLocale.ts
@@ -0,0 +1,25 @@
+import client from 'part:@sanity/base/client';
+
+export async function isUniqueInLocale({
+	slug,
+	context,
+	type,
+}: {
+	slug: any;
+	context: any;
+	type?: string;
+}) {
+	const { document } = context;
+	const id = document._id.replace(/^drafts\./, '');
+	const params = {
+		draft: `drafts.${id}`,
+		published: id,
+		lang: document.__i18n_lang,
+		slug,
+	};
+	const typeQuery = type ? `_type == ${type} && ` : '';
+	const query = `!defined(*[${typeQuery}__i18n_lang == $lang && !(_id in [$draft, $published]) && slug.current == $slug][0]._id)`;
+	const result = await client.fetch(query, params);
+
+	return result;
+}
