9fe1d731b10efb32330627c65864e79d54d26ad8
Bram Smulders <source@bram.is>
2023-02-07 18:05:56 +0000

    remove yarn dependency

--- packages/cms/src/utilities/getViews.tsx
+++ packages/cms/src/utilities/getViews.tsx
@@ -1,15 +1,5 @@
-import { MapPreview } from './MapPreview';
-// import { DocumentJsonPreview } from './views/DocumentJsonPreview';
-// import { WebPreview } from './views/WebPreview';
-
 export const getViews = (S, type: 'page' | 'document', schemaType: string) => {
-	const views = [
-		S.view.form(),
-		// S.view.component(DocumentJsonPreview).title('JSON'),
-	];
-
-	// if (type === 'page')
-	// 	views.push(S.view.component(WebPreview).title('Preview'));
+	const views = [S.view.form()];
 
 	return views;
 };
