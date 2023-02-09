6010396cc43b908f11c74317b7d140ecbd56c8a0
Bram Smulders <source@bram.is>
2023-02-09 16:12:14 +0100

    added flexible button object

---
+++ packages/cms/src/schemas/translated/objects/button.ts
@@ -0,0 +1,52 @@
+import { defineType, defineField } from 'sanity';
+
+export default defineType({
+	title: 'Knop',
+	name: 'flexibleButton',
+	type: 'object',
+	fields: [
+		defineField({
+			title: 'Label',
+			name: 'label',
+			type: 'string',
+			validation: (Rule) => Rule.required(),
+		}),
+		// TODO: add internal link selector
+		defineField({
+			title: 'href',
+			name: 'href',
+			type: 'string',
+		}),
+		defineField({
+			title: 'Variant',
+			name: 'variant',
+			type: 'string',
+			initialValue: 'primary',
+			options: {
+				layout: 'dropdown',
+				list: [
+					{
+						title: 'Primary',
+						value: 'primary',
+					},
+					{
+						title: 'Secondary',
+						value: 'secondary',
+					},
+					{
+						title: 'Tertiary',
+						value: 'tertiary',
+					},
+					{
+						title: 'Quaternary',
+						value: 'quaternary',
+					},
+				],
+			},
+		}),
+	],
+	options: {
+		collapsible: true,
+		collapsed: false,
+	},
+});
