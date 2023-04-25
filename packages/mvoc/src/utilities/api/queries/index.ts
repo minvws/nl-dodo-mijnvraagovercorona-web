173472ec9831b28e2adc50bb8cd0615bd12d6c26
William Smits <wp.smits@minvws.nl>
2023-04-25 14:43:51 +0200

    clean up exports

--- packages/mvoc/src/utilities/api/queries/index.ts
+++ packages/mvoc/src/utilities/api/queries/index.ts
@@ -2,6 +2,4 @@ export * from './page';
 export * from './image';
 export * from './modals';
 export * from './localeProperty';
-// export * from './siteSettings';
 export * from './metaData';
-// export * from './tip';
