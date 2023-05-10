d4f78ba4919c7ac91bf2152821d953020ec65ee7
Bram Smulders <source@bram.is>
2023-05-10 14:21:45 +0200

    look at parent instead of document

--- packages/cms/src/utilities/filterTaleReferenceByPageReference.ts
+++ packages/cms/src/utilities/filterTaleReferenceByPageReference.ts
@@ -1,10 +1,11 @@
 export const filterTaleReferenceByPageReference = async ({
 	document,
+	parent,
 	getClient,
 }) => {
 	// get pageReference and its connected tales
 	const pageReference = await getClient({ apiVersion: '2023-05-10' }).fetch(
-		`*[_id == "${document.pageReference._ref}"][0] {taleCollection}`,
+		`*[_id == "${parent.pageReference._ref}"][0] {taleCollection}`,
 	);
 
 	// Transform taleCollection to a array of id's
