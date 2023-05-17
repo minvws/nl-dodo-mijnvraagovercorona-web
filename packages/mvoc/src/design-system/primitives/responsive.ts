2b6d1c5cb6ec43692d818c368609dc8dcba79530
Bram Smulders <source@bram.is>
2023-05-17 17:46:47 +0200

    added medium MQ's

--- packages/mvoc/src/design-system/primitives/responsive.ts
+++ packages/mvoc/src/design-system/primitives/responsive.ts
@@ -1,4 +1,7 @@
+export const mqMedium = '(min-width: 44.5em)'; // 712px
+export const mqMediumUntil = '(max-width: 44.4375em)'; // 711px
+
 export const mqLarge = '(min-width: 64em)'; // 1024px
-export const mqLargeUntil = '(min-width: 63.9375em)'; // 1023px
+export const mqLargeUntil = '(max-width: 63.9375em)'; // 1023px
 
 export const mqBlockSmall = '(min-height: 41em)'; // 656px
