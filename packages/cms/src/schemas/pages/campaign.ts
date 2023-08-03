eeef77c82bd73e57b6584f7ec39d1fc902e6e34f
William Smits <wp.smits@minvws.nl>
2023-08-03 08:53:08 +0000

    Feature/mvoc 494/mvoc 498 campagnepagina type

---
+++ packages/cms/src/schemas/pages/campaign.ts
@@ -0,0 +1,91 @@
+import { isUniqueInLocale } from '../../utilities/isUniqueInLocale';
+import { defineType, defineField } from 'sanity';
+import { filterReferenceByLanguage } from '../../utilities/filterReferenceByLanguage';
+
+async function isUnique(slug: any, context: any) {
+	return await isUniqueInLocale({
+		slug,
+		context,
+		type: 'campaign-page',
+	});
+}
+
+export default defineType({
+	title: 'Campagne pagina',
+	name: 'campaign-page',
+	type: 'document',
+	initialValue: {
+		__i18n_lang: 'nl',
+	},
+	fieldsets: [{ name: 'urlStructure', title: 'Url structuur' }],
+	fields: [
+		defineField({
+			title: 'Meta data',
+			name: 'metaData',
+			type: 'metaData',
+		}),
+
+		defineField({
+			title: 'Hero',
+			name: 'hero',
+			type: 'hero',
+		}),
+
+		defineField({
+			title: 'Hulp per onderwerp',
+			name: 'support',
+			type: 'reference',
+			to: [{ type: 'support' }],
+		}),
+
+		defineField({
+			title: 'Hulp',
+			name: 'assistance',
+			type: 'reference',
+			to: [{ type: 'assistance' }],
+			options: {
+				filter: filterReferenceByLanguage,
+			},
+		}),
+
+		defineField({
+			title: 'Sub pagina referentie',
+			description:
+				'Genereert deze pagina onder een andere pagina in de hierachie',
+			name: 'subFolderReference',
+			type: 'pageSourceSelector',
+			fieldset: 'urlStructure',
+		}),
+
+		defineField({
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			fieldset: 'urlStructure',
+			validation: (Rule) => Rule.required(),
+			options: {
+				source: 'metaData.title',
+				isUnique: isUnique,
+			},
+		}),
+	],
+	preview: {
+		select: {
+			title: 'hero.title',
+			locale: '__i18n_lang',
+			slug: 'slug.current',
+			subFolderReferenceSlug: 'subFolderReference.slug.current',
+			media: 'hero.image',
+		},
+		prepare(selection) {
+			const { title, locale, slug, subFolderReferenceSlug, media } = selection;
+			return {
+				title: title,
+				subtitle: `/${locale}${
+					subFolderReferenceSlug ? `/…/${subFolderReferenceSlug}` : ''
+				}/${slug}`,
+				media,
+			};
+		},
+	},
+});
