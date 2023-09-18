58baf5d486ddb665a1dc8aba5aab79e932a33c9f
Bram Smulders <source@bram.is>
2023-09-18 15:14:39 +0000

    [MVOC-793] hoofdmenu breakpoint

--- packages/mvoc/src/design-system/primitives/responsive.ts
+++ packages/mvoc/src/design-system/primitives/responsive.ts
@@ -4,4 +4,7 @@ export const mqMediumUntil = '(max-width: 44.4375em)'; // 711px
 export const mqLarge = '(min-width: 64em)'; // 1024px
 export const mqLargeUntil = '(max-width: 63.9375em)'; // 1023px
 
+export const mqHuge = '(min-width: 80em)'; // 1280px
+export const mqHugeUntil = '(max-width: 79.9375em)'; // 1279px
+
 export const mqBlockSmall = '(min-height: 41em)'; // 656px
