c6229cf65679849a77ba84b39fbf5b48f0f05690
Bram Smulders <source@bram.is>
2023-11-28 12:06:32 +0100

    Moved Button & ButtonTool to @mvoc/ui/elements

--- packages/mvoc/src/design-system/components/ContentBlock/index.ts
+++ packages/mvoc/src/design-system/components/ContentBlock/index.ts
@@ -1,5 +1,3 @@
 export { default as ContentBlock } from './ContentBlock.astro';
-export { default as MultiContentBlockOld } from './MultiContentBlockOld.astro';
 export { default as MultiContentBlock } from './MultiContentBlock.astro';
 export type { Props as ContentBlockProps } from './ContentBlock.astro';
-export type { MultiContentBlockOldProps } from './MultiContentBlockOld.astro';
