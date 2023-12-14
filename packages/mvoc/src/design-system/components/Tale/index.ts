e5846d1bcbd589e93f20dfbfb4935c481a32f499
Bram Smulders <source@bram.is>
2023-12-14 08:59:22 +0000

    [MVOC-530/MVOC-1102/MVOC-1107] Tale & Picture

--- packages/mvoc/src/design-system/components/Tale/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TaleList } from './TaleList.astro';
-export { default as Tale } from './Tale.astro';
---
+++ packages/ui/src/lib/components/Tale/index.ts
@@ -0,0 +1,2 @@
+export { default as Tale } from './Tale.astro';
+export type { Props as TaleProps } from './Tale.astro';
