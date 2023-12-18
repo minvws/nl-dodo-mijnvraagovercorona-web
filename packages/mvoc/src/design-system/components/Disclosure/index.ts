03da94dfedd30aa364fa103e9f0e673631382c90
Bram Smulders <source@bram.is>
2023-12-18 10:40:26 +0000

    [MVOC-530/MVOC-1103&MVOC-1108] Disclosure & MediaBlock

--- packages/mvoc/src/design-system/components/Disclosure/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Disclosure } from './Disclosure.astro';
-export { default as DisclosureItem } from './DisclosureItem.astro';
-export { default as DisclosureItemCampaign } from './DisclosureItemCampaign.astro';
