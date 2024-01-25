c7d89d32f00fc81731808fa0a74e48eadbd8ee31
Bram Smulders <source@bram.is>
2024-01-25 10:48:15 +0100

    Removed unused function

--- packages/mvoc/src/utilities/locale/translation.ts
+++ packages/mvoc/src/utilities/locale/translation.ts
@@ -23,21 +23,3 @@ export const prefixUrlWithlocale = (href: string, locale: Locale) =>
 	!href.startsWith('mailto:')
 		? `${locale.urlPrefix}/${href.replace(/^\//, '')}`
 		: href;
-
-export const getCurrentUrlForLocale = (
-	path: string,
-	/**
-	 * Requested locale
-	 */
-	locale: Locale,
-	/**
-	 * Locale page is currently viewed in
-	 */
-	currentLocale: Locale,
-) => {
-	// If current page is only '/', replace it with the url prefix.
-	// This makes the '/' homepage also redirect to '/nl'
-	return path === '/'
-		? locale.urlPrefix
-		: path.replace(currentLocale.urlPrefix, locale.urlPrefix);
-};
