cbf0d6d54b230ffd10f7b29ed12813c59adbddd9
Bram Smulders <source@bram.is>
2024-01-30 14:45:43 +0100

    Moved Steps to ui

--- packages/mvoc/src/components/Card/index.ts
+++ packages/mvoc/src/components/Card/index.ts
@@ -1,3 +1,2 @@
 export { default as CardDay } from './CardDay.astro';
-export { default as Steps } from './Steps.astro';
 export { default as CardPrivacy } from './CardPrivacy.astro';
--- packages/ui/src/lib/components/Card/index.ts
+++ packages/ui/src/lib/components/Card/index.ts
@@ -3,3 +3,4 @@ export { default as CardAdvice } from './CardAdvice.astro';
 export { default as CardSupport } from './CardSupport.astro';
 export { default as CardBlock } from './CardBlock.astro';
 export { default as CardDisclosure } from './CardDisclosure.astro';
+export { default as Steps } from './Steps.astro';
